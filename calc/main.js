d29c4069d4398b0a3d2b503130f1a38fe17103b5
Dionisio-Fernandez <33870327+Dionisio-Fernandez@users.noreply.github.com>
2021-10-12 11:30:06 -0300

    Update main.js

--- calc/main.js
+++ calc/main.js
@@ -1,33 +1,74 @@
-function addEquation() {
-  let li = document.createElement("li")
-  let inp = new MathfieldElement()
-  li.appendChild(inp)
-  li.classList.add("eq")
-  inp.setAttribute("virtual-keyboard-mode", "manual")
-  inp.setAttribute("options", "{smartFence:false, fontsDirectory: 'node_modules/mathlive/dist/fonts'}")
-  inp.classList.add("inp")
+document.querySelector(".linear.button._1").onclick = addLinearEquation;
+document.querySelector(".linear.button._2").onclick = solveLinearEquation;
+document.querySelector(".linear.button._3").onclick = removeLinearEquation;
 
-  document.querySelector(".list").appendChild(li)
+const domData = {
+  linear: { //Linear Equation Solver
+    input: function() {
+      let el = new MathfieldElement();
+      setAttributes(el, {"virtual-keyboard-mode": "manual"});
+      el.classList.add("linear", "input");
+      return el;
+    },
+    listItem: function() {
+      let el = document.createElement("li");
+      el.classList.add("linear", "list-item");
+      el.appendChild(domData.linear.input());
+      return el;
+    },
+    listQuery: ".linear.list",
+    mainContainerQuery: ".linear.main-container",
+  },
+  quadratic: {
+    input: function() {
+      let el = new MathfieldElement();
+      setAttributes(el, {"virtual-keyboard-mode": "manual"});
+      el.classList.add("quadratic", "input");
+      return el;
+    },
+    variable: function(power) {
+      let el = document.createElement("span")
+      setAttributes(el, {})
+      el.classList.add("quadratic", "variable")
+      el.innerHTML = `x<sup>${power}</sup>`
+      return el
+    },
+    monomial: function(power) {
+      let el = document.createElement("div");
+      setAttributes(el, {});
+      el.classList.add("quadratic", "monomial")
+      el.appendChild(domData.quadratic.input())
+      el.appendChild(domData.quadratic.variable(power))
+      return el
+    }
+  }
 }
-function removeEquation() {
-  let li = document.querySelector(".list")
-  let ch = li.children
-  li.classList.add("eq")
-  ch[ch.length-1].remove()
+function setAttributes(el, data) {
+  if(!el || Object.keys(data).length === 0) {return el}
+  for(let k in data) {
+    el.setAttribute(k, data[k]);
+  }
+  return el;
 }
-function beta(a, b) {
+function addLinearEquation() {
+  document.querySelector(`${domData.linear.listQuery}`).appendChild(domData.linear.listItem())
+}
+function removeLinearEquation() {
+  document.querySelector(`${domData.linear.listQuery}`).lastElementChild.remove()
+}
+function termSort(a, b) {
   return a.replaceAll(/[^a-z]/g, "") > b.replaceAll(/[^a-z]/g, "") // [1b, -3a] -> [-3a, 1b]
 }
-function alpha() {
+function solveLinearEquation() {
   let eqlst = [];
-  [...(document.querySelector(".list").children)].forEach(x => eqlst.push(x.children[0].value))
+  [...(document.querySelector(`${domData.linear.listQuery}`).children)].forEach(x => eqlst.push(x.children[0].value))
   
   let amatrix = eqlst.map(x => x.split("=")[0])
   amatrix = amatrix.map(x => x.replaceAll(/\\frac\{(.*?)\}\{(.*?)\}/g, "$1/$2"))
   let bmatrix = eqlst.map(x => parseFloat(x.split("=")[1]))
   let unkSym = new Set()
   amatrix = amatrix.map(s => {
-    return s.replaceAll(/-/g, "+-").split("+").sort(beta).join("+")
+    return s.replaceAll(/-/g, "+-").split("+").sort(termSort).join("+")
   })
   
   amatrix = amatrix.map(s => s.split("+").filter(y => y).map(y => {
@@ -35,8 +76,6 @@ function alpha() {
     let scope = {unk: 1}
     unkSym.add(sym)
     y=y.replaceAll(sym, "unk")
-    
-    
     return math.evaluate(y, scope)
   }))
   let solved 
@@ -45,6 +84,4 @@ function alpha() {
   solved = solved.map((x, i) => `${unkSym[i]} = ${x}\n`)
   alert(solved.join(""))
 }
-document.querySelector("#btn1").onclick = addEquation
-document.querySelector("#btn2").onclick = removeEquation
-document.querySelector("#btn3").onclick = alpha
+
